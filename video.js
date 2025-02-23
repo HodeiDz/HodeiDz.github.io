0d6e44fd9c8fee840db978c7512d368e27a3fd53
Hodei <148000557+HodeiDz@users.noreply.github.com>
2025-02-23 11:50:33 +0100

    add unified video media html and fix visual animation error

---
+++ video.js
@@ -0,0 +1,67 @@
+  const pages = [
+    '2A.1', '2A.2', '2A.3',
+    '2B.1', '2B.2', '2B.3',
+    '2D.1', '2D.2', '2D.3',
+    '2E.1', '2E.2', '2E.3'
+  ];
+
+  const titles = [
+    /* 2A.1 */       '2A.1', 
+    /* 2A.2 */       'HARRAPATUAK', 
+    /* 2A.3 */       '2A.3',
+    /* 2B.1 */       '2B.1', 
+    /* 2B.2 */       '2B.2', 
+    /* 2B.3 */       '2B.3',
+    /* 2D.1 */       '2D.1', 
+    /* 2D.2 */       '2D.2', 
+    /* 2D.3 */       '2D.3',
+    /* 2E.1 */       '2E.1', 
+    /* 2E.2 */       '2E.2', 
+    /* 2E.3 */       '2E.3'
+  ];
+
+  const url = [
+    /* 2A.1 */       '2A.1', 
+    /* 2A.2 */       'https://drive.google.com/file/d/1uXj7qQShYfi3ir--hIveJ6aKOY6D19Mc/preview', 
+    /* 2A.3 */       '2A.3',
+    /* 2B.1 */       '2B.1', 
+    /* 2B.2 */       '2B.2', 
+    /* 2B.3 */       '2B.3',
+    /* 2D.1 */       '2D.1', 
+    /* 2D.2 */       '2D.2', 
+    /* 2D.3 */       '2D.3',
+    /* 2E.1 */       '2E.1', 
+    /* 2E.2 */       '2E.2', 
+    /* 2E.3 */       '2E.3'
+  ];
+
+window.onload = function() {
+
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get('videoid');
+
+    if (!page) { //! if no attribute to default
+        window.location.href = 'video_media.html?videoid=2A.1';
+    }
+    
+    localStorage.setItem('videoid', page);
+    console.log(page);
+
+    var index = pages.indexOf(page);
+    console.log('DEBUG: RAW INDEX: ' + index);
+
+    document.getElementById('title').textContent = `${titles[index]}`;
+    document.getElementById('video').src = `${url[index]}`;
+    document.title = titles[index] + ' | 5. Zinemaldia';
+
+
+
+};
+
+function nextVideo() {
+    
+}
+
+function prevVideo() {
+
+}
